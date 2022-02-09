2bcea02bafd9b07df80e923e3aeb8afefc963c0d
GitHub Action <action@github.com>
2022-02-09 10:40:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/saloed/pluginconfigsample/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/saloed/pluginconfigsample/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.saloed.pluginconfigsample.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.saloed.pluginconfigsample.MyBundle
 
 class MyApplicationService {
 
