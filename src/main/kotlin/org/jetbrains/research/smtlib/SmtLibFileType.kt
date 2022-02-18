b141acfebbca02b904442177ba7eef3213aa6753
Saloed <napster_1997@mail.ru>
2022-02-18 11:41:11 +0300

    change file extension to .smt2

--- src/main/kotlin/org/jetbrains/research/smtlib/SmtLibFileType.kt
+++ src/main/kotlin/org/jetbrains/research/smtlib/SmtLibFileType.kt
@@ -3,7 +3,7 @@ package org.jetbrains.research.smtlib
 import com.intellij.openapi.fileTypes.LanguageFileType
 
 object SmtLibFileType : LanguageFileType(SmtLibLanguage) {
-    const val FILE_EXTENSION = "smtlib"
+    const val FILE_EXTENSION = "smt2"
     override fun getName() = "SmtLib file"
     override fun getDescription() = "SmtLib file"
     override fun getDefaultExtension() = FILE_EXTENSION
